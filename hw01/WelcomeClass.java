4797ca07e78914d438cd65f92d582303989e7d0d
chijioke2018 <ccu218@lehigh.edu>
2014-09-03 02:26:08 +0000

    First commit

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,20 @@
+//define class WelcomeClass
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String args[]) {
+        //print statement
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("^  ^  ^  ^  ^  ^");
+        System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-C--C--U--2--1--8-|");
+        System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("v  v  v  v  v  v");
+        
+        System.out.println("This year, I am beginning my college career at Lehigh University.");
+        
+    }
+    
+}
